6c1827d5f0cf9fdf6eb730e80bbdae1b99b551f9
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-25 13:17:06 -0300

    feat<jwt.service>: Migrate jwt functions to new structure

---
+++ projects/back/src/infra/jwt/jwt.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
+
+
+export interface JwtSignConfig {
+  expires_in: number;
+}
+
+export interface JwtVerify<T> {
+  status: 'VALID' | 'INVALID';
+  payload: T | undefined;
+}
+
+@Injectable()
+export class JwtService {
+  
+  sign<T extends string | Buffer | object>(data: T, SECRET: string, config: JwtSignConfig): string {
+    const signOptions: any = { expiresIn: config.expires_in };  // Correctly defining options
+    return jwt.sign(data, SECRET, signOptions);
+  }
+
+  verify<T>(token: string, SECRET: string): JwtVerify<T> {
+    try {
+      const decoded = jwt.verify(token, SECRET);
+      return {
+        status: 'VALID',
+        payload: decoded as T,
+      };
+    } catch (_error) {
+      return {
+        status: 'INVALID',
+        payload: undefined,
+      };
+    }
+  }
+}
