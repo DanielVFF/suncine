b10d635a0b14e7ccef09ec5a20b1ca24946b9aab
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-25 13:16:35 -0300

    feat<seed>: Added firstUser seed to seed service

--- projects/back/src/infra/seed/seed.module.ts
+++ projects/back/src/infra/seed/seed.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { EnvironmentConfigService } from './';
 import { SeedService } from './seed.service';
+import { AuthService } from 'src/app/auth/auth.service';
+import { SecretService } from '../secrets/secrets.service';
+import { JwtService } from '../jwt/jwt.service';
+import { UserRepository } from '../db/repositories/user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { userSchema } from '../db/models/user.model';
 
 @Module({
-      providers: [EnvironmentConfigService, SeedService],
+      imports: [
+            MongooseModule.forFeature([{ name: 'User', schema: userSchema }])
+      ],
+      providers: [AuthService, SecretService, JwtService, SeedService , UserRepository],
       exports: []
 })
-export class SeedModule {
-
-}
+export class SeedModule { }
--- projects/back/src/infra/seed/seed.service.ts
+++ projects/back/src/infra/seed/seed.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { AuthService } from 'src/app/auth/auth.service';
+import { SecretService } from '../secrets/secrets.service';
+import { EnvironmentConfigService } from '../enviroment-config/environment-config.service';
+import { UserData } from '../db/models/user.model';
 
 @Injectable()
-export class SeedService {}
+export class SeedService implements OnApplicationBootstrap {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly secretService: SecretService,
+    private readonly environmentService: EnvironmentConfigService,
+  ) { }
+
+  async onApplicationBootstrap() {
+    await this.seed();
+  }
+
+  private async seed() {
+    await this.firstUser()
+  }
+
+  private async firstUser() {
+    const { hash, salt } = await this.secretService.encrypt(
+      this.environmentService.getPepper(),
+      this.environmentService.getUserPass()
+    )
+    const user_raw: Omit<UserData, "id"> = {
+      login: this.environmentService.getUserLogin(),
+      password: hash,
+      salt: salt,
+      name: "Dev"
+    }
+    this.authService.fetchByLoginOrCreate(user_raw)
+    console.info(`First User's Seed executed`)
+  }
+}
