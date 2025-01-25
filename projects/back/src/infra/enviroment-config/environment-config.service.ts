b075b099396b2d88ac5cf662966a641ffe5edf01
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-25 13:22:37 -0300

    chore<environment-config-service>: Added missing .env vars

--- projects/back/src/infra/enviroment-config/environment-config.service.ts
+++ projects/back/src/infra/enviroment-config/environment-config.service.ts
@@ -2,14 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvironmentConfigService {
+export class EnvironmentConfigService // implements
+{
   constructor(private configService: ConfigService) {}
 
-  // Como não usamos as variaveis de conexão separadamente faz mais sentido separar assim, e podemos ajustar erros de conexão pelo env
   getMongoUrl(): string {
     const dbHost = this.configService.get<string>('MONGO_URL');
     if (!dbHost) {
       throw new Error('MONGO_URL não está definido no .env');
+    }
+    return dbHost;
+  }
+
+  getPepper(): string {
+    const dbHost = this.configService.get<string>('PEPPER');
+    if (!dbHost) {
+      throw new Error('PEPPER is not defined in .env file');
+    }
+    return dbHost;
+  }
+
+  getUserLogin(): string {
+    const dbHost = this.configService.get<string>('DEFAULT_USER_LOGIN');
+    if (!dbHost) {
+      throw new Error('DEFAULT_USER_LOGIN is not defined in .env file');
+    }
+    return dbHost;
+  }
+  getUserPass(): string {
+    const dbHost = this.configService.get<string>('DEFAULT_USER_PASS');
+    if (!dbHost) {
+      throw new Error('DEFAULT_USER_PASS is not defined in .env file');
     }
     return dbHost;
   }
