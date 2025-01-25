d26015b0ed37acc3ca06af22198049f0e85499f6
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-25 13:23:09 -0300

    feat<secrets,service>: Created secrets service

---
+++ projects/back/src/infra/secrets/secrets.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import * as argon2 from '@node-rs/argon2';
+import { nanoid } from 'nanoid'; // Nanoid agora pode ser importado diretamente.
+
+@Injectable()
+export class SecretService {
+  async encrypt(PEPPER: string, data: string) {
+    const SALT = nanoid();
+    const SEASONED = data + SALT + PEPPER;
+    const HASH = await argon2.hash(SEASONED);
+    return { hash: HASH, salt: SALT };
+  }
+
+  async validate(SALT: string, PEPPER: string, HASH: string, data: string): Promise<boolean> {
+    const SEASONED = data + SALT + PEPPER;
+    return await argon2.verify(HASH, SEASONED);
+  }
+}
