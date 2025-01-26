2bba71b63bf7db9d0aeff8e5474c5cffc4e9b69f
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-26 16:54:54 -0300

    feat<user.controller>: Created user/me route

--- projects/back/src/app/user/user.controller.ts
+++ projects/back/src/app/user/user.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from 'src/infra/guards/auth.guard';
+import { CustomFastifyRequest } from 'src/interfaces/config/custom-fastify-request.interface';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService){}
 
-    @Get()
+    @Get('me')
     @UseGuards(AuthGuard)
-    async findMany(){
-        return await this.userService.findAll()
+    async findMany(@Req() request: CustomFastifyRequest){
+        return {
+            status: "OK",
+            payload : await this.userService.fetchByLogin(request.user.login)
+        }
     }
 }
