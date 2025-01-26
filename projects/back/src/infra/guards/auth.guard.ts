e39c31c2aa70f224654550d80557ade4937825ba
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-26 11:40:56 -0300

    chore<env.ts>: Removed remaining env_var calls

--- projects/back/src/infra/guards/auth.guard.ts
+++ projects/back/src/infra/guards/auth.guard.ts
@@ -1,12 +1,13 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { FastifyRequest } from 'fastify';
-import { ENV_VAR } from '../../../env';
 import { User } from 'src/interfaces/user/user.interface';
 import { JwtService } from '../jwt/jwt.service';
+import { EnvironmentConfigService } from '../enviroment-config/environment-config.service';
+import { CustomFastifyRequest } from 'src/interfaces/config/custom-fastify-request.interface';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly jwt : JwtService){} 
+  constructor(private readonly jwt: JwtService, private readonly environmentConfigService: EnvironmentConfigService) { }
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest<FastifyRequest>();
@@ -17,13 +18,13 @@ export class AuthGuard implements CanActivate {
     }
 
     const [, token] = tokenBearered.split(' ');
-    const { status, payload } = this.jwt.verify<User>(token, ENV_VAR.JWT_TOKEN);
+    const { status, payload } = this.jwt.verify<User>(token, this.environmentConfigService.getJwtToken());
 
-    if (status === 'INVALID') {
+    if (status === 'INVALID' && payload == undefined) {
       throw new UnauthorizedException('Invalid token');
     }
-
-    (request as any).user = payload;
+    (request as CustomFastifyRequest).user = payload as User;
     return true;
+
   }
 }
