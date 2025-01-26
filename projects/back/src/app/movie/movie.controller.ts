6a2ffb997e5b8852555cd3c486680a4884a14b15
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-26 16:35:10 -0300

    chore<movie.controller>: Alterando função de like para buscar do dto

--- projects/back/src/app/movie/movie.controller.ts
+++ projects/back/src/app/movie/movie.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Put, Req, UseGuards } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { AuthGuard } from 'src/infra/guards/auth.guard';
 import { CustomFastifyRequest } from 'src/interfaces/config/custom-fastify-request.interface';
+import { LikeOrUnlikeMovieDto } from 'src/dto/like-or-unlike.dto';
 
 @Controller('movie')
 @UseGuards(AuthGuard)
@@ -15,17 +16,20 @@ export class MovieController {
     async mostTrended( @Req() request: CustomFastifyRequest){
         return await this.movieService.mostTrended(request.user.id)
     }
-
     
     @Get('top-10')
     async top10Movies( @Req() request: CustomFastifyRequest){
         return await this.movieService.top10Movies(request.user.id)
     }
 
+    @Get('likes')
+    async likedMovies( @Req() request: CustomFastifyRequest){
+        return await this.movieService.likedMovies(request.user.id)
+    }
 
     @Put('like/:movieId')
-    async likeOrUnlikeMovie(@Param('movieId') movieId : string , @Req() request: CustomFastifyRequest): Promise<any>{
-        return await this.movieService.likeOrUnlikeMovie(request.user.id, movieId)
+    async likeOrUnlikeMovie(@Param() params: LikeOrUnlikeMovieDto , @Req() request: CustomFastifyRequest): Promise<{status : 'LIKE' | 'DISLIKE'}>{
+        return await this.movieService.likeOrUnlikeMovie(request.user.id, params.movieId)
     }
 
 }
