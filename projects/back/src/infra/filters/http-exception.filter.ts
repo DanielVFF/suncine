3973b499ff1b001cbec546f233fc0d99f8622210
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-26 16:53:19 -0300

    chore<auth-guards && http-exception-filter>: Modified http validation to match given swagger

--- projects/back/src/infra/filters/http-exception.filter.ts
+++ projects/back/src/infra/filters/http-exception.filter.ts
@@ -1,32 +1,34 @@
 import {
-    ExceptionFilter,
-    Catch,
-    ArgumentsHost,
-    HttpException,
-  } from '@nestjs/common';
-  import { Request, Response } from 'express';
-  
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { FastifyReply } from 'fastify';
 
-  @Catch(HttpException)
-  export class HttpExceptionFilter implements ExceptionFilter {
-    catch(exception: HttpException, host: ArgumentsHost) {
-      const ctx = host.switchToHttp();
-      const response = ctx.getResponse<Response>();
-      const request = ctx.getRequest<Request>();
-      const status = exception.getStatus();
-  
-      const exceptionResponse = exception.getResponse();
-      const message =
-        typeof exceptionResponse === 'string'
-          ? exceptionResponse
-          : (exceptionResponse as { message: string }).message;
-  
-      response.status(status).json({
-        statusCode: status,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-        message,
-      });
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<FastifyReply>();
+    const status = exception.getStatus();
+
+    const exceptionResponse = exception.getResponse();
+    const message = exceptionResponse
+
+    const statusMap = {
+      400: "BAD REQUEST",
+      401: "UNAUTHORIZED",
+      404: "NOT FOUND",
+      500: 'ERROR',
+    };
+
+    const responseFormated : {status: string, message?: string} = {
+      status: statusMap[status] ?? status,
     }
+
+    if(message && typeof message == 'string') responseFormated.message = message
+
+    response.status(status).send(responseFormated);
   }
-  
+}
