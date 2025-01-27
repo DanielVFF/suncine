eab6e3fef6e549a732270c2d76b06303ce90f2e7
Daniel Vitor <114309972+DanielVFF@users.noreply.github.com>
2025-01-26 21:56:58 -0300

    feat<user>: Added register user route

--- projects/back/src/app/user/user.module.ts
+++ projects/back/src/app/user/user.module.ts
@@ -5,12 +5,13 @@ import { UserRepository } from 'src/infra/db/repositories/user.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from 'src/infra/db/models/user.model';
 import { JwtService } from 'src/infra/jwt/jwt.service';
+import { SecretService } from 'src/infra/secrets/secrets.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: userSchema }])
   ],
-  providers: [UserService, UserRepository, JwtService],
+  providers: [UserService, UserRepository, JwtService, SecretService],
   controllers: [UserController],
   exports: [UserService]
 })
---
+++ projects/back/src/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsString, MinLength, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3, { message: 'The login must be at least 3 characters long.' })
+  @MaxLength(20, { message: 'The login can be at most 20 characters long.' })
+  login: string;
+
+  @IsString()
+  @MinLength(8, { message: 'The password must be at least 8 characters long.' })
+  password?: string;
+
+  @IsString()
+  @MinLength(3, { message: 'The name must be at least 3 characters long.' })
+  @MaxLength(50, { message: 'The name can be at most 50 characters long.' })
+  name: string;
+
+
+  salt?: string
+}
